0e7d0499eec41047f69379ffb6e5f4db5468c831
Rubiel Alejandro González Labarta <74202468+ralabarta@users.noreply.github.com>
2022-01-24 21:49:41 +0100

    Add files via upload

---
+++ examples/example.php
@@ -0,0 +1,71 @@
+<?php
+
+use PKPass\PKPass;
+
+require('../vendor/autoload.php');
+
+// Replace the parameters below with the path to your .p12 certificate and the certificate password!
+$pass = new PKPass('../Certificates.p12', 'password');
+
+// Pass content
+$data = [
+    'description' => 'Demo pass',
+    'formatVersion' => 1,
+    'organizationName' => 'Flight Express',
+    'passTypeIdentifier' => 'pass.com.scholica.flights', // Change this!
+    'serialNumber' => '12345678',
+    'teamIdentifier' => 'KN44X8ZLNC', // Change this!
+    'boardingPass' => [
+        'primaryFields' => [
+            [
+                'key' => 'origin',
+                'label' => 'San Francisco',
+                'value' => 'SFO',
+            ],
+            [
+                'key' => 'destination',
+                'label' => 'London',
+                'value' => 'LHR',
+            ],
+        ],
+        'secondaryFields' => [
+            [
+                'key' => 'gate',
+                'label' => 'Gate',
+                'value' => 'F12',
+            ],
+            [
+                'key' => 'date',
+                'label' => 'Departure date',
+                'value' => '07/11/2012 10:22',
+            ],
+        ],
+        'backFields' => [
+            [
+                'key' => 'passenger-name',
+                'label' => 'Passenger',
+                'value' => 'John Appleseed',
+            ],
+        ],
+        'transitType' => 'PKTransitTypeAir',
+    ],
+    'barcode' => [
+        'format' => 'PKBarcodeFormatQR',
+        'message' => 'Flight-GateF12-ID6643679AH7B',
+        'messageEncoding' => 'iso-8859-1',
+    ],
+    'backgroundColor' => 'rgb(32,110,247)',
+    'logoText' => 'Flight info',
+    'relevantDate' => date('Y-m-d\TH:i:sP')
+];
+$pass->setData($data);
+
+// Add files to the pass package
+$pass->addFile('images/icon.png');
+$pass->addFile('images/icon@2x.png');
+$pass->addFile('images/logo.png');
+
+// Create and output the pass
+if(!$pass->create(true)) {
+    echo 'Error: ' . $pass->getError();
+}
